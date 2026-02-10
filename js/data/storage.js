40615ca8e8f260f3b928afd8d2aa673028ba1c4d
NamikaseUzumaki067 <namikazeuzumaki67@gmail.com>
2026-02-10 10:07:17 -0400

    fix: alinhar api e storage v2 e centralizar cliente supabase

--- js/data/storage.js
+++ js/data/storage.js
@@ -1,4 +1,12 @@
 // js/data/storage.js
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
+// 🔐 Supabase (centralizado aqui)
+const SUPABASE_URL = "https://ljhgeoetyvhbafewnmgw.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_85mJLqObjWFtZLFhefNm3w_b7o7sqZX";
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
 import {
   apiGetPedidos,
   apiCreatePedido,
